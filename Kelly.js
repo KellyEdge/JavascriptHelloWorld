09ea80198a4f3bc07bfa59c5b500dacf98dcf8c7
kellyedge <edge.kelly@gmail.com>
2017-07-04 16:49:31 -0500

    initial commit

---
+++ Kelly.js
@@ -0,0 +1,21 @@
+/**
+ * Created by kellyedge on 6/4/17.
+ */
+
+$( document ).ready(function() {
+
+    /* event binding
+     document.getElementById("btnHelloWorld").onclick = function() {
+     window.alert("Hello World!")
+     }*/
+
+    $("#btnHelloWorld").click(function(){
+        alert("The button was clicked.");
+    });
+
+});
+
+
+
+
+
